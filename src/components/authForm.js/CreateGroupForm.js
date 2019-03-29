10ae85bc526f05666dfd73479ea097ddc95fc831
saud9030 <saud.amakn@gmail.com>
2019-03-29 14:16:28 +0300

    created CreateGroupForm

---
+++ src/components/authForm.js/CreateGroupForm.js
@@ -0,0 +1,112 @@
+import React, { Component } from "react";
+import apiUrl from "../../apiConfig";
+import { getUser } from "../../services/AuthService";
+class CreateGroup extends Component {
+  state = {
+    formData: {
+      name: null,
+      city: null,
+      leader: null,
+      founded: null,
+      description: null,
+      contact_number: null,
+      email: null,
+      type: null
+    },
+    err: null
+  };
+
+  handleLoginRequest = group => {
+    let url = `${apiUrl}/api/group`;
+
+    console.log({ email: getUser().email, group });
+    console.log(url);
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ leader: getUser().name, group })
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.error) this.setState({ err: data.error });
+        this.props.changeActivePage("home");
+      })
+      .catch(e => console.log(e));
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.handleLoginRequest(this.state.formData);
+  };
+
+  handleChange = ({ currentTarget }) => {
+    const formData = { ...this.state.formData };
+    formData[currentTarget.name] = currentTarget.value;
+    this.setState({ formData });
+  };
+
+  render() {
+    return (
+      <div className="pt-5 mt-5">
+        <h1>create new group</h1>
+
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label>Name of the group</label>
+            <input
+              name="name"
+              className="form-control"
+              type="name"
+              onChange={this.handleChange}
+            />
+            <label>Which city are you in? </label>
+            <input
+              name="city"
+              className="form-control"
+              onChange={this.handleChange}
+            />
+            <label>When the group was founded? </label>
+            <input
+              name="founded"
+              type="date"
+              className="form-control"
+              onChange={this.handleChange}
+            />
+            <label>Tell us about the group </label>
+            <input
+              name="description"
+              className="form-control"
+              onChange={this.handleChange}
+            />
+            <label>group's number </label>
+            <input
+              name="contact_number"
+              className="form-control"
+              onChange={this.handleChange}
+            />
+            <label>Group's email </label>
+            <input
+              name="new"
+              type="password"
+              className="form-control"
+              onChange={this.handleChange}
+            />
+            <label>what kind of volunteer group are you? </label>
+            <input
+              name="type"
+              className="form-control"
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Change Password
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default CreateGroup;
