3b00141d4ce8de9ece1faa632f891e4ae531fa90
saud9030 <saud.amakn@gmail.com>
2019-04-03 09:38:00 +0300

    fixed a bug that affected the signout when the user is in the groupView page, and now it redirect the user when they signout to the homepage

--- src/components/authForm.js/viewGroups.js
+++ src/components/authForm.js/viewGroups.js
@@ -8,6 +8,7 @@ class ViewGroups extends Component {
     name: "",
     activeGroup: null
   };
+
   // this function to show the detalis of a specific group
   showGroup = activeGroup => {
     this.setState({ activeGroup });
@@ -65,81 +66,85 @@ class ViewGroups extends Component {
   };
   render() {
     let something = [];
-    // if (this.state.groups.length > 0) {
-    something = this.state.groups.map(group => {
-      const userIds = group.UserGroups.map(userGroup => userGroup.user_id);
-      // to check if the user is already a member of this group or not
-      const userIsInGroup = userIds.includes(getUser().id);
-      return (
-        <React.Fragment>
-          <tr>
-            <td>{group.name}</td>
-            <td>{group.city}</td>
-            <td>
-              <button onClick={() => this.showGroup(group.id)}>show</button>
-              <div
-                className={
-                  group.id === this.state.activeGroup ? "show" : "hide"
-                }
-              >
-                <h1>{group.name} </h1>
-                <div class="container">
-                  <div class="row">
-                    <div class="col">city: {group.city}</div>
-                    <div class="col">first started: {group.founded}</div>
-                  </div>
-                  <div class="row">
-                    <div class="col">description: {group.description}</div>
-                  </div>
-                  <div class="row">
-                    <div class="col">email: {group.email}</div>
-                    <div class="col">number: {group.contact_number}</div>
-                    <div class="col">type of orgniaztion: {group.type}</div>
-                  </div>
-                </div>
-                <button onClick={this.hideGroup}> close </button>
-              </div>
-            </td>
-            <td>
-              {userIsInGroup ? (
-                <button onClick={this.leaveGroup} value={group.id}>
-                  Delete
-                </button>
-              ) : (
-                <button onClick={this.joinGroup} value={group.id}>
-                  Join
-                </button>
-              )}
-            </td>
-          </tr>
-        </React.Fragment>
-      );
-    });
-    //   console.log("this mine", something[0]);
-    // } else console.log("dd");
-
-    return (
-      <div>
-        <table class="table table-sm table-dark">
-          <thead>
+    if (getUser()) {
+      something = this.state.groups.map(group => {
+        const userIds = group.UserGroups.map(userGroup => userGroup.user_id);
+        // to check if the user is already a member of this group or not
+        const userIsInGroup = userIds.includes(getUser().id);
+        return (
+          <React.Fragment>
             <tr>
-              {/* <th scope="col">#</th> */}
-              <th scope="col">Name</th>
-              <th scope="col">City</th>
-              <th scope="col">Show</th>
-              <th scope="col">join</th>
+              <td>{group.name}</td>
+              <td>{group.city}</td>
+              <td>
+                <button onClick={() => this.showGroup(group.id)}>show</button>
+                <div
+                  className={
+                    group.id === this.state.activeGroup ? "show" : "hide"
+                  }
+                >
+                  <h1>{group.name} </h1>
+                  <div class="container">
+                    <div class="row">
+                      <div class="col">city: {group.city}</div>
+                      <div class="col">first started: {group.founded}</div>
+                    </div>
+                    <div class="row">
+                      <div class="col">description: {group.description}</div>
+                    </div>
+                    <div class="row">
+                      <div class="col">email: {group.email}</div>
+                      <div class="col">number: {group.contact_number}</div>
+                      <div class="col">type of orgniaztion: {group.type}</div>
+                    </div>
+                  </div>
+                  <button onClick={this.hideGroup}> close </button>
+                </div>
+              </td>
+              <td>
+                {userIsInGroup ? (
+                  <button onClick={this.leaveGroup} value={group.id}>
+                    Delete
+                  </button>
+                ) : (
+                  <button onClick={this.joinGroup} value={group.id}>
+                    Join
+                  </button>
+                )}
+              </td>
             </tr>
-          </thead>
-          <tbody>{something}</tbody>
-        </table>
-        {/* <div className={this.state.pop}>
+          </React.Fragment>
+        );
+      });
+      // }
+      //   console.log("this mine", something[0]);
+      // } else console.log("dd");
+
+      return (
+        <div>
+          <table class="table table-sm table-dark">
+            <thead>
+              <tr>
+                {/* <th scope="col">#</th> */}
+                <th scope="col">Name</th>
+                <th scope="col">City</th>
+                <th scope="col">Show</th>
+                <th scope="col">join</th>
+              </tr>
+            </thead>
+            <tbody>{something}</tbody>
+          </table>
+          {/* <div className={this.state.pop}>
           <h1>name </h1>
           {this.state.groups.city}
 
           <button onClick={this.hideGroup}> close </button>
         </div> */}
-      </div>
-    );
+        </div>
+      );
+    } else {
+      window.location.reload();
+    }
   }
 }
 
