cde9aa587d63e0fe62f43444998447d4ba2ffca7
saud9030 <saud.amakn@gmail.com>
2019-04-03 13:47:04 +0300

    added footer with social media links

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import CreatGroupForm from "./components/authForm.js/CreateGroupForm";
 import Home from "./components/Home";
 import ViewGroups from "./components/authForm.js/viewGroups";
 import ViewEvents from "./components/authForm.js/viewEvents";
+import twitter from "./twitter.svg";
+import linkedin from "./linkedin.svg";
+import github from "./github.svg";
+
 // import Profile from "./components/Profile";
 class App extends Component {
   state = {
@@ -37,7 +41,7 @@ class App extends Component {
   render() {
     const { user, activePage } = this.state;
     return (
-      <div>
+      <div id="mainDiv">
         <Nav
           user={user}
           changeActivePage={this.changeActivePage}
@@ -84,6 +88,28 @@ class App extends Component {
           {/* when the user log in , user will go to the home page **home should be changed when there is something to show in profile */}
           {/* {activePage === "profile" ? <Home /> : ""} */}
         </div>
+
+        <footer class="page-footer font-small cyan darken-3">
+          <div class="footer-copyright text-center py-3">
+            © 2018 Copyright:
+            <a href="https://mdbootstrap.com/education/bootstrap/">
+              {" "}
+              MDBootstrap.com
+            </a>
+          </div>
+          <span> created by Saud Almutairi </span>
+          <div>
+            <a href="https://twitter.com/saud95t">
+              <img className="contactMe" src={twitter} alt="myTwitterPage" />
+            </a>
+            <a href="https://github.com/saud9030">
+              <img className="contactMe" src={github} alt="myGithubPage" />
+            </a>
+            <a href="https://www.linkedin.com/saud8almutairi/">
+              <img className="contactMe" src={linkedin} alt="myLinkedinPage" />
+            </a>
+          </div>
+        </footer>
       </div>
     );
   }
